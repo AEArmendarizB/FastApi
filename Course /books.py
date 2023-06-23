190bd3071c80d05015ccdd9a2dc55a9af5cd6341
Andres Armendariz <andy_armendariz@hotmail.es>
2023-06-23 12:41:18 -0500

    path parameters

--- Course /books.py
+++ Course /books.py
@@ -16,3 +16,13 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.get("/books/{book_tittle}")
+async def read_book(book_tittle: str):
+    for book in BOOKS:
+        if book.get('tittle').casefold()==book_tittle.casefold():
+            return book
+            
+
+            
