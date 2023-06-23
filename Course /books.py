90fc67813c0b932d6acb70ad0cbb90718b6306b2
Andres Armendariz <andy_armendariz@hotmail.es>
2023-06-23 14:55:00 -0500

    post request

--- Course /books.py
+++ Course /books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 
 app = FastAPI()
@@ -41,3 +41,8 @@ async def read_author_category_by_query(book_author:str, category:str):
             book.get('category').casefold() == category.casefold():
                 book_to_return.append(book)
     return book_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
